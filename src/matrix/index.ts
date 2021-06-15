b89f01acfa11880c864d9549bf9b286720bec6b9
ramanlukashyk <rlukashyk@evolution.com>
2021-06-15 12:35:30 +0300

    Add checking matrix shape compatibility

--- src/matrix/index.ts
+++ src/matrix/index.ts
@@ -1,4 +1,5 @@
 import { identity } from '../utils/function'
+import { reverse, zip } from '../utils/array'
 
 export type Matrix1 = number[]
 export type Matrix2 = number[][]
@@ -43,6 +44,20 @@ export const shape = <
 export const divide = <T extends MatrixN>(matrix: T, scalar: number): T =>
   matrix.map((x) => isMatrix(x) ? divide(x, scalar) : x / scalar) as T
 
+export const multiply = <
+  T1 extends MatrixN,
+  T2 extends MatrixN,
+  T3 extends MatrixN,
+>(matrix1: T1, matrix2: T2): T3 => {
+  const shape1 = shape(matrix1)
+  const shape2 = shape(matrix2)
+
+  if (!shapeCompatible(shape1, shape2)) {
+    throw new Error('Matrix could not be broadcast together.')
+  }
+
+}
+
 export const sample = <
   T extends MatrixN,
   S extends (
@@ -56,3 +71,13 @@ export const sample = <
   d0 ? matrix.slice(d0[0], d0[1]).map((item) => isMatrix(item) ? sample(item, ...rest) : item) as T : matrix
 
 const isMatrix = (value: MatrixN | number): value is MatrixN => Array.isArray(value)
+
+/**
+ * To make sure two matrix are compatible their dimensions should be compared.
+ * Comparing starts with the trailing (i.e. rightmost) dimensions and works its way left.
+ * Two dimensions are compatible when:
+ * 1. they are equal
+ * 2. one of them is 1
+*/
+const shapeCompatible = <T1 extends VectorN, T2 extends VectorN>(shape1: T1, shape2: T2): boolean =>
+  zip(reverse(shape1), reverse(shape2)).every(([a, b]) => a === b || a === 1 || b === 1)
