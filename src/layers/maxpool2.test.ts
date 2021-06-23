4268f1ef8217bdd07d8a866a47399f240ade7647
ramanlukashyk <rlukashyk@evolution.com>
2021-06-23 12:57:33 +0300

    Add backward method for maxpool2 layer

--- src/layers/maxpool2.test.ts
+++ src/layers/maxpool2.test.ts
@@ -1,12 +1,19 @@
 import { conv3x3, forward as conv3x3Forward } from './conv3x3'
-import { shape } from '../matrix'
+import { randn, shape } from '../matrix'
 import { image } from './data'
-import { forward } from './maxpool2'
+import { forward, backward } from './maxpool2'
 
 describe('maxpool2', () => {
-  it('works properly', () => {
+  it('forward', () => {
     const output3x3 = conv3x3Forward(image, conv3x3(8))
     const output = forward(output3x3)
     expect(shape(output)).toEqual([13, 13, 8])
+  })
+
+  it('backward', () => {
+    const input3x3 = randn(26, 26, 8)
+    const gradient = randn(13, 13, 8)
+    const output = backward(input3x3, gradient)
+    expect(shape(output)).toEqual([26, 26, 8])
   })
 })
--- src/layers/maxpool2.ts
+++ src/layers/maxpool2.ts
@@ -1,15 +1,39 @@
-import { Matrix1, Matrix3, max, partition, shape } from '../matrix'
+import { at, Matrix3, max, partition, shape, zeros } from '../matrix'
 import { range } from '../utils/array'
 import { idivide } from '../utils/math'
 
 export const forward = (input: Matrix3): Matrix3 => {
-  const [h, w, n] = shape(input)
+  const [h, w, f] = shape(input)
   return range(0, idivide(h, 2)).map((i) =>
     range(0, idivide(w, 2)).map((j) =>
-      max(frame2x2xN(input, i, j, n), [0, 1]) as Matrix1
-    )
+      max(frame2x2xN(input, i, j, f), [0, 1]),
+    ),
   )
 }
 
-const frame2x2xN = (input: Matrix3, i: number, j: number, n: number): Matrix3 =>
-  partition(input, [i * 2, i * 2 + 2], [j * 2, j * 2 + 2], [0, n])
+export const backward = (input: Matrix3, gradient: Matrix3): Matrix3 => {
+  const [h, w, f] = shape(input)
+  const dLdInput = zeros(h, w, f)
+
+  range(0, idivide(h, 2)).forEach((i) =>
+    range(0, idivide(w, 2)).forEach((j) => {
+      const frame = frame2x2xN(input, i, j, f)
+      const amax = max(frame, [0, 1])
+
+      frame.forEach((x, i1) => {
+        x.forEach((y, j1) => {
+          y.forEach((z, f1) => {
+            if (z === at(amax, f1)) {
+              dLdInput[i * 2 + i1][j * 2 + j1][f1] = at(at(at(gradient, i), j), f1)
+            }
+          })
+        })
+      })
+    }),
+  )
+
+  return dLdInput
+}
+
+const frame2x2xN = (input: Matrix3, i: number, j: number, f: number) =>
+  partition(input, [i * 2, i * 2 + 2], [j * 2, j * 2 + 2], [0, f])
