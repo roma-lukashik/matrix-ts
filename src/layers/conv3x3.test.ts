38bda12b36eb92c7e5a600f3c2cbc5cf4326ae24
ramanlukashyk <rlukashyk@evolution.com>
2021-06-29 18:06:20 +0300

    Remove hardcoded test data

--- src/layers/conv3x3.test.ts
+++ src/layers/conv3x3.test.ts
@@ -1,18 +1,19 @@
 import { Conv3x3 } from './conv3x3'
 import { randn, shape } from '../matrix'
-import { image } from './data'
 
 describe('conv3x3', () => {
+  const input = randn(28, 28)
+
   it('#forward', () => {
     const conv3x3 = new Conv3x3(8)
-    const output = conv3x3.forward(image)
+    const output = conv3x3.forward(input)
     expect(shape(output)).toEqual([26, 26, 8])
   })
 
   it('#backward', () => {
     const conv3x3 = new Conv3x3(8)
     const gradient = randn(26, 26, 8)
-    const output = conv3x3.backward(image, gradient, 0.05)
+    const output = conv3x3.backward(input, gradient, 0.05)
     expect(shape(output)).toEqual([8, 3, 3])
   })
 })
