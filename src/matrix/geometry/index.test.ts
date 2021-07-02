77762907d8db97534c5fba59f5c07a5c3bef5002
ramanlukashyk <rlukashyk@evolution.com>
2021-07-02 14:44:53 +0300

    Fix reshape operator

--- src/matrix/geometry/index.test.ts
+++ src/matrix/geometry/index.test.ts
@@ -93,6 +93,21 @@ describe('geometry', () => {
       ])
     })
 
+    it('converts (24, 0) matrix to (2, 3, 2, 2) matrix', () => {
+      expect(reshape(arange(24), [2, 3, 2, 2])).toEqual([
+        [
+          [[0, 1], [2, 3]],
+          [[4, 5], [6, 7]],
+          [[8, 9], [10, 11]],
+        ],
+        [
+          [[12, 13], [14, 15]],
+          [[16, 17], [18, 19]],
+          [[20, 21], [22, 23]],
+        ],
+      ])
+    })
+
     it('throws an error in case when shapes are not compatible', () => {
       const m = [[1, 2], [3, 4]]
       expect(() => reshape(m, [2, 3])).toThrowError('Incompatible shape (2,3) for reshaping of (2,2) matrix.')
