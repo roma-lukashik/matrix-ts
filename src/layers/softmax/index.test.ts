4d688305480da9947a3c81007549a9e76e27945b
ramanlukashyk <rlukashyk@evolution.com>
2021-07-05 12:38:17 +0300

    Add softmax tests

--- src/layers/softmax/index.test.ts
+++ src/layers/softmax/index.test.ts
@@ -1,18 +1,40 @@
 import { Softmax } from '.'
-import { create, shape } from '../../matrix'
-import { normal } from '../../utils/random'
+import { arange, reshape } from '../../matrix'
 
 describe('Softmax', () => {
-  const input = create(normal, 13, 13, 8)
+  const [h, w, f] = [2, 2, 4]
+  const inputLength = h * w * f
+  const outputLength = 10
+  const input = reshape(arange(inputLength), [h, w, f])
+  const inc = (start: number) => () => start += 0.1
 
   it('#forward', () => {
-    const softmax = new Softmax({ inputLength: 13 * 13 * 8, outputLength: 10 })
-    expect(shape(softmax.forward(input))).toEqual([10])
+    const softmax = new Softmax({
+      inputLength,
+      outputLength,
+      weightInitializer: inc(0),
+    })
+    expect(softmax.forward(input)).toEqualMatrix([
+      0.001, 0.001, 0.003, 0.006, 0.012, 0.026, 0.056, 0.118, 0.249, 0.528,
+    ])
   })
 
   it('#backward', () => {
-    const softmax = new Softmax({ inputLength: 13 * 13 * 8, outputLength: 10 })
+    const softmax = new Softmax({
+      inputLength,
+      outputLength,
+      weightInitializer: inc(0),
+    })
     const gradient = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-    expect(shape(softmax.backward(input, gradient, 0.005))).toEqual([13, 13, 8])
+    expect(softmax.backward(input, gradient, 0.005)).toEqualMatrix([
+      [
+        [-0.00031895, -0.00031895, -0.00031895, -0.00031895],
+        [-0.00031895, -0.00031895, -0.00031895, -0.00031895],
+      ],
+      [
+        [-0.00031895, -0.00031895, -0.00031895, -0.00031895],
+        [-0.00031895, -0.00031895, -0.00031895, -0.00031895],
+      ],
+    ])
   })
 })
