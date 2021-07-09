f74e3e26a34f31d011b40a8900fbe655194911ac
ramanlukashyk <rlukashyk@evolution.com>
2021-07-09 12:32:20 +0300

    Code refactoring

--- src/matrix/aggregation/index.ts
+++ src/matrix/aggregation/index.ts
@@ -1,9 +1,10 @@
 import { nonzero } from '../../utils/math'
-import { arrlen, empty, first } from '../../utils/array'
-import { Matrix, Matrix0, MatrixDimensions, MatrixN, NLevelNestedMatrix, VectorN } from '../utils/types'
+import { arrlen } from '../../utils/array'
+import { Matrix, Matrix0, MatrixDimensions, MatrixN, NLevelNestedMatrix } from '../utils/types'
 import { arange } from '../creation'
 import { isMatrix0, matrixn, ndim } from '../geometry'
 import { add, broadcast, MatrixBinaryOperator, multiply } from '../binary-operation'
+import { isDefined } from '../../utils/function'
 
 type AggregateMatrixOperator = {
   <T extends Matrix>(matrix: T): Matrix0
@@ -29,20 +30,16 @@ const aggregate = <
   T1 extends Matrix,
   T2 extends MatrixDimensions<T1>,
   T3 extends NLevelNestedMatrix<T1, T2>,
->(matrix: T1, axes: T2, operator: MatrixBinaryOperator): T3 => {
-  if (isMatrix0(matrix) || empty(axes)) {
+>(matrix: T1, [d0, ...dn]: T2, operator: MatrixBinaryOperator): T3 => {
+  if (isMatrix0(matrix) || !isDefined(d0)) {
     return matrix as unknown as T3
   }
-  return aggregate(
-    aggregateNesting(matrix, first(axes), operator),
-    reduceAxes(axes),
-    operator,
-  ) as T3
+  const nesting = aggregateNesting(matrix, d0, operator)
+  const rest = dn.map((x) => x > d0 ? x - 1 : x)
+  return aggregate(nesting, rest, operator) as T3
 }
 
 const aggregateNesting = (matrix: MatrixN, axis: number, operator: MatrixBinaryOperator): Matrix =>
   nonzero(axis) && ndim(matrix) > 1 ?
     matrix.map((x) => aggregateNesting(matrixn(x), axis - 1, operator)) :
     matrix.reduce(operator)
-
-const reduceAxes = ([axis, ...axes]: VectorN): VectorN => axes.map((x) => x > axis ? x - 1 : x)
